2358a5846b3b72c733a5a36941b7dc9849838de2
aristov <vv.aristov@gmail.com>
2017-01-15 15:20:50 +0300

    wip

---
+++ docs/pipik.js
@@ -0,0 +1,45 @@
+{
+    const { SVGDOMAssembler } = svgmodule
+    const rectlist = Array.from(document.querySelectorAll('rect'))
+    const colorpixelset = rectlist.reduce((res, rect) => {
+        const fill = rect.getAttribute('fill')
+        const position = [rect.getAttribute('x') || 0, rect.getAttribute('y') || 0]
+        const positionlist = res[fill]
+        if(!positionlist) {
+            res[fill] = [position]
+        } else {
+            positionlist.push(position)
+        }
+        return res
+    }, {})
+
+    class MoveRect extends SVGDOMAssembler {
+        constructor(node, init) {
+            super(node, init)
+            this.init({
+                tabIndex : 0,
+                onkeydown : event => {
+                    const { key } = event
+                    if(key.startsWith('Arrow')) {
+                        this.onArrowKey(event)
+                        event.preventDefault()
+                    }
+                }
+            })
+        }
+        onArrowKey({ key, target }) {
+            switch(key) {
+                case 'ArrowUp': this.y--; break
+                case 'ArrowDown': this.y++; break
+                case 'ArrowLeft': this.x--; break
+                case 'ArrowRight': this.x++; break
+            }
+            target.parentNode.append(target)
+            target.focus()
+        }
+    }
+    const moverectmap = rectlist.map(rect => new MoveRect(rect))
+
+    console.log(moverectmap)
+    // console.log(JSON.stringify(colorpixelset))
+}
